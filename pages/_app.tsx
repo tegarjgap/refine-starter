de6a3749d7bf21dfb371e5fed7c7d14a5ec02235
tegarjgap <tegarjagatgeni@gmail.com>
2023-06-27 22:36:44 +0700

    Initial commit from create-refine-app

---
+++ pages/_app.tsx
@@ -0,0 +1,59 @@
+import { GitHubBanner, Refine } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/nextjs-router";
+import type { NextPage } from "next";
+import { AppProps } from "next/app";
+
+import { Layout } from "@components/layout";
+import dataProvider from "@refinedev/simple-rest";
+import "@styles/global.css";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  noLayout?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
+  const renderComponent = () => {
+    if (Component.noLayout) {
+      return <Component {...pageProps} />;
+    }
+
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    );
+  };
+
+  return (
+    <>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <Refine
+          routerProvider={routerProvider}
+          dataProvider={dataProvider(API_URL)}
+          options={{
+            syncWithLocation: true,
+            warnWhenUnsavedChanges: true,
+          }}
+        >
+          {renderComponent()}
+          <RefineKbar />
+          <UnsavedChangesNotifier />
+          <DocumentTitleHandler />
+        </Refine>
+      </RefineKbarProvider>
+    </>
+  );
+}
+
+export default MyApp;
