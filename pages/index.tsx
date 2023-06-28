2d433d511a8b5048962755ca2200e2aa23801b19
tegarjgap <tegarjagatgeni@gmail.com>
2023-06-28 07:18:30 +0700

    feat: add menubar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import { CardWithForm } from "@components/card";
+import { MenubarDemo } from "@components/menubar";
 import { WelcomePage } from "@refinedev/core";
 
 export default function Index() {
   return (
     <>
-      <h1 className="text-3xl font-bold  bg-red-400 text-center">
-        Test shadcn/ui
-      </h1>
+      <div>
+        <MenubarDemo />
+      </div>
       <div className="flex justify-center items-center mt-4">
         <CardWithForm />
       </div>
